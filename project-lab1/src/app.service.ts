7cbf52b7226fee611e79e1f2433fedaf150c6423
SerhiyKotyash <remkirades@gmail.com>
2021-12-06 01:59:38 +0200

    lab4

--- project-lab1/src/app.service.ts
+++ project-lab1/src/app.service.ts
@@ -3,31 +3,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 
-  getHello(): Promise<any> {
-    let a,b;
-    a =12342;
-    b = 20000;
-    const res = this.TwoNumber(a,b);
-    return res;
+  getHello(): Student {  
+    const stud = new Student("Сергій","Котяш",2001,2,122,"бакалавріат","-");
+    return stud;
+    
   }
-
-  public async TwoNumber(a:number,b:number):Promise<any>{
-     let one:string = "Число" + ".\n\n" + a + ".\n\n" + "більше ніж" + ".\n\n" + b;
-     let two:string = "Число" + ".\n\n" + b + ".\n\n" + "більше ніж" + ".\n\n" + a;
-
-     if (a > b) {
-       return one; 
-     }
-     else{
-      return two;
-     }
-
-
-  }
-
-
-
   
- 
+}
+export class Student {
+  namef:string;
+  namel:string;
+  rkna:number;
+  course:number;
+  prof:number;
+  stupin:string;
+  obsh:string;
 
+  constructor(namef,namel,rkna,course,prof,stupin,obsh){
+          this.namef =namef;
+          this.namel =namel;
+          this.rkna =rkna;
+          this.course =course;
+          this.prof =prof;
+          this.stupin =stupin;
+          this.obsh =obsh;
+  }
 }
